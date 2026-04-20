6aa9beb82b4cee6bc3f988d06724647ec36382b1
YaswanthKumar15 <dasariyeshwanthkumar@gmail.com>
2026-04-20 17:44:14 +0530

    Time: 2 ms (50.59%), Space: 19.1 MB (91.95%) - LeetHub

--- 3866-first-unique-even-element/3866-first-unique-even-element.py
+++ 3866-first-unique-even-element/3866-first-unique-even-element.py
@@ -3,13 +3,13 @@ class Solution:
         cnt=0
         dic={}
         for i in range(len(nums)):
-            if nums[i] in dic:
-                dic[nums[i]]+=1
-            else:
-                dic[nums[i]]=1
-        for keys,values in dic.items():
-            if keys%2 == 0 and values == 1:
+            if nums[i]%2==0:
+                if nums[i] in dic:
+                    dic[nums[i]]+=1
+                else:
+                    dic[nums[i]]=1
+        for keys,items in dic.items():
+            if keys % 2 == 0 and items == 1:
                 cnt=keys
                 break
-        return cnt if cnt>0 else -1
-        
+        return cnt if cnt > 0 else -1
