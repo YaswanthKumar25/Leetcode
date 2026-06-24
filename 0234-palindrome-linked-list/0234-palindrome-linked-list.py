920512406e5788a14d55c7711680d9a9a00b777e
YaswanthKumar15 <dasariyeshwanthkumar@gmail.com>
2026-06-24 06:56:26 +0530

    Time: 50 ms (16.77%), Space: 42.2 MB (96.53%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,13 +5,27 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        arr=[]
-        temp=head
-        while temp:
-            arr.append(temp.val)
-            temp=temp.next
-        if arr==arr[::-1]:
-            return True
-        else:
-            return False
+        slow=fast=head
+        while fast.next != None and fast.next.next != None:
+            slow=slow.next
+            fast=fast.next.next
+        new_node=self.reverse(slow.next)
+        first=head
+        second=new_node
+        while second != None:
+            if first.val != second.val:
+                return False
+            first=first.next
+            second=second.next
+        return True
+
+    def reverse(self,head):
+        prev=None
+        curr=head
+        while curr!=None:
+            next_node=curr.next
+            curr.next=prev
+            prev=curr
+            curr=next_node
+        return prev
         
