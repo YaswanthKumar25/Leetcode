2c0217dafc081d1b5ae96d7871055e469bd1e5fc
YaswanthKumar15 <dasariyeshwanthkumar@gmail.com>
2026-08-06 13:03:31 +0530

    Time: 11 ms (82.46%), Space: 21.7 MB (92.77%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,19 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # Initialize a standard Python dictionary
-        anagram_map = {}
-        
+        hash={}
         for word in strs:
-            # 1. Sort the word and join it back into a string to use as a key
-            sorted_word = "".join(sorted(word))
-            
-            # 2. Check if the sorted key is already in the dictionary
-            if sorted_word in anagram_map:
-                # If it exists, append the original word to its list
-                anagram_map[sorted_word].append(word)
+            rev="".join(sorted(word))
+            if rev in hash:
+                hash[rev].append(word)
             else:
-                # If it doesn't exist, create a new key with a list containing the word
-                anagram_map[sorted_word] = [word]
-                
-        # 3. Return all the grouped lists from the dictionary
-        return list(anagram_map.values())
+                hash[rev]=[word]
+        arr=[]
+        for i in hash.values():
+            arr.append(i)
+        return arr
